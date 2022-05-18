c74d9414213be10b7aecda5542bc4dcb1f61a36c
NatalyM333 <nataly.minziuk@gmail.com>
2022-05-19 01:29:47 +0300

    Create DTO

---
+++ TestTask.BLL/DTO/AccountcontactDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.BLL.DTO
+{
+    public class AccountcontactDTO
+    {
+        [Required]
+        public string AccountName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\.0-9]+@[a-z]+\.[a-z]{2,3}$",
+            ErrorMessage = "Sorry, only letters (a-z), numbers (0-9), and periods (.) are allowed for email")]
+        public string ContactEmail { get; set; }
+    }
+}
---
+++ TestTask.BLL/DTO/ContactAccountIncidentDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.BLL.DTO
+{
+    public class ContactAccountIncidentDTO
+    {
+        [Required]
+        public string AccountName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\.0-9]+@[a-z]+\.[a-z]{2,3}$",
+            ErrorMessage = "Sorry, only letters (a-z), numbers (0-9), and periods (.) are allowed for email")]
+        public string ContactEmail { get; set; }
+        [Required]
+        public string IncidentDescription { get; set; }
+    }
+}
